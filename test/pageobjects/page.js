da0e46d736aea64c607c8365b85c2d2875b80bab
Oriol Gómez <oriol.gomez@wiris.com>
2023-11-06 11:30:48 +0100

    test: add a test to open the mt demo develoeprs page

--- test/pageobjects/page.js
+++ test/pageobjects/page.js
@@ -9,7 +9,7 @@ module.exports = class Page {
     * Opens a sub page of the page
     * @param path path of the sub page (e.g. /path/to/page.html)
     */
-    open () {
-        return browser.url()
+    open (path) {
+        return browser.url(path)
     }
 }
