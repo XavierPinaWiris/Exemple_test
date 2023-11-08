22e0ae1a90d6c37c00b23aceaf088104a9793caa
Oriol Gómez <oriol.gomez@wiris.com>
2023-11-08 09:52:00 +0100

    refactor: cookie acceptance function

--- test/pageobjects/demo.page.js
+++ test/pageobjects/demo.page.js
@@ -1,11 +1,7 @@
 import Page from './page.js';
 
 class MTDemoPage extends Page {
-    get cookiebtn () {
-        return $('#onetrust-accept-btn-handler')
-    }
-
-    get arabicbtn () {
+    get arabicNotationButton () {
         return $('#new_tab_container > div > a:nth-child(3)')
     }
     open () {
--- test/specs/demo.e2e.js
+++ test/specs/demo.e2e.js
@@ -1,14 +1,10 @@
 import demoPage from '../pageobjects/demo.page.js';
 
 describe('Demo test', () => {
-    it('The cookie button is clicked correctly', async () => {
-        await demoPage.open()
-        await demoPage.cookiebtn.click()
-
-    })
     it('The Arabic Notation button is clicked correctly', async () => {
-        console.log(demoPage.cookiebtn)
-        await demoPage.arabicbtn.click()
+        await demoPage.open();
+        await demoPage.acceptCookies();
+        await demoPage.arabicNotationButton.click()
     })
 })
 
