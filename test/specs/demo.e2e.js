d25ed24b8be9de2116d3ee3dfcf034b9a7f6c011
xpina-at-wiris <xavier.pina@wiris.com>
2023-11-08 09:34:09 +0100

    Allure integration

--- test/specs/demo.e2e.js
+++ test/specs/demo.e2e.js
@@ -1,9 +1,15 @@
 import demoPage from '../pageobjects/demo.page.js';
 
+var allure = require('allure-commandline');
+var generation = allure(['generate', 'allure-results']);
+
 describe('My Login application', () => {
     it('should login with valid credentials', async () => {
         await demoPage.open()
         await browser.debug()
     })
+    generation.on('exit', function(exitCode) {
+        console.log('Generation is finished with code:', exitCode);
+    });
 })
 
