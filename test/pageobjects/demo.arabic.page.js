5b498502cb285b0602cd2306a24b3f6cbd90026e
xpina-at-wiris <xavier.pina@wiris.com>
2023-11-09 10:17:32 +0100

    Toolbar visibility tested for each page

--- test/pageobjects/demo.arabic.page.js
+++ test/pageobjects/demo.arabic.page.js
@@ -2,6 +2,10 @@ import Page from './page.js';
 
 class MTDemoArabicPage extends Page {
 
+    get arabicNotationToolbar () {
+        return $('#editorContainerChem > div')
+    }
+
     open () {
         return super.open('arabic.php');
     }
--- test/specs/demo.e2e.js
+++ test/specs/demo.e2e.js
@@ -1,5 +1,7 @@
 import demoDevPage from '../pageobjects/demo.developers.page.js';
 import demoArabPage from '../pageobjects/demo.arabic.page.js';
+import demoChemtypePage from '../pageobjects/demo.chemtype.page.js';
+import demoIndexPage from '../pageobjects/demo.index.page.js';
 
 describe('Demo test', () => {
     
@@ -19,3 +21,70 @@ describe('Demo test', () => {
     })
 })
 
+describe('Developers Toolbar Visibility', () => {
+    it('The simple toolbar is displayed', async () => {
+        await demoDevPage.open();
+
+        await expect(demoDevPage.simpleToolbar).toBeDisplayed()
+    })
+
+    it('The custom toolbar is displayed', async () => {
+        await demoDevPage.open();
+
+        await expect(demoDevPage.customToolbar).toBeDisplayed()
+    })
+
+    it('The PARCC toolbar is displayed', async () => {
+        await demoDevPage.open();
+
+        await expect(demoDevPage.parccToolbar).toBeDisplayed()
+    })
+
+    it('The publisher toolbar is displayed', async () => {
+        await demoDevPage.open();
+
+        await expect(demoDevPage.publisherToolbar).toBeDisplayed()
+    })
+
+    it('The format toolbar is displayed', async () => {
+        await demoDevPage.open();
+
+        await expect(demoDevPage.formatExampleToolbar).toBeDisplayed()
+    })
+
+    it('The mathML and LaTeX toolbar is displayed', async () => {
+        await demoDevPage.open();
+
+        await expect(demoDevPage.mathML_LaTeX_ExampleToolbar).toBeDisplayed()
+    })
+
+    it('The accessibility toolbar is displayed', async () => {
+        await demoDevPage.open();
+
+        await expect(demoDevPage.accessibilityToolbar).toBeDisplayed()
+    })
+})
+
+describe('Arabic Notation Toolbar Visibility', () => {
+    it('The arabic notation toolbar is displayed', async () => {
+        await demoArabPage.open();
+
+        await expect(demoArabPage.arabicNotationToolbar).toBeDisplayed()
+    })
+})
+
+describe('ChemType Toolbar Visibility', () => {
+    it('The ChemType toolbar is displayed', async () => {
+        await demoChemtypePage.open();
+
+        await expect(demoChemtypePage.chemTypeToolbar).toBeDisplayed()
+    })
+})
+
+describe('Index Toolbar Visibility', () => {
+    it('The index toolbar is displayed', async () => {
+        await demoIndexPage.open();
+
+        await expect(demoIndexPage.indexToolbar).toBeDisplayed()
+    })
+})
